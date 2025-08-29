95734641b195dbbacb23f3611117f2d256e28f93
Ethaga <108997005+Ethaga@users.noreply.github.com>
2025-08-29 09:27:34 +0100

    Update doma-api.ts

--- lib/doma-api.ts
+++ lib/doma-api.ts
@@ -1,9 +1,8 @@
-query ExampleQuery {
-  chainStatistics {
-    totalActiveWallets
-    totalTransactions
-    totalNamesTokenized
-    totalNamesTokenizedCumulative
-    totalRevenueUsd
-  }
+{  "name": null,
+  "tokensName2": null,  "tokenId": null,
+  "correlationId": null,  "nameActivitiesName2": null,
+  "tokenActivitiesTokenId2": null,  "nameStatisticsTokenId2": null,
+  "timeRange": null,  "dailyTransactionCountTimeRange2": null,
+  "dailyCumulativeTokenizationCountTimeRange2": null,  "dailyTokenizationCountTimeRange2": null,
+  "dailyRevenueTimeRange2": nullllllll
 }
