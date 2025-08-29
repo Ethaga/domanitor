972de3f7923331288c4f2109f52df792207cf3f6
Ethaga <108997005+Ethaga@users.noreply.github.com>
2025-08-29 09:28:32 +0100

    Update doma-api.ts

--- lib/doma-api.ts
+++ lib/doma-api.ts
@@ -1,8 +1,56 @@
-{  "name": null,
-  "tokensName2": null,  "tokenId": null,
-  "correlationId": null,  "nameActivitiesName2": null,
-  "tokenActivitiesTokenId2": null,  "nameStatisticsTokenId2": null,
-  "timeRange": null,  "dailyTransactionCountTimeRange2": null,
-  "dailyCumulativeTokenizationCountTimeRange2": null,  "dailyTokenizationCountTimeRange2": null,
-  "dailyRevenueTimeRange2": nullllllll
+query ExampleQuery($name: String!, $tokensName2: String!, $tokenId: String!, $correlationId: String!, $nameActivitiesName2: String!, $tokenActivitiesTokenId2: String!, $nameStatisticsTokenId2: String!, $timeRange: StatisticsTimeRange!, $dailyTransactionCountTimeRange2: StatisticsTimeRange!, $dailyCumulativeTokenizationCountTimeRange2: StatisticsTimeRange!, $dailyTokenizationCountTimeRange2: StatisticsTimeRange!, $dailyRevenueTimeRange2: StatisticsTimeRange!) {
+  names {
+    
+  }
+  name(name: $name) {
+    
+  }
+  tokens(name: $tokensName2) {
+    
+  }
+  token(tokenId: $tokenId) {
+    
+  }
+  fractionalTokens {
+    
+  }
+  fractionalTokenBalances {
+    
+  }
+  command(correlationId: $correlationId) {
+    
+  }
+  nameActivities(name: $nameActivitiesName2) {
+    
+  }
+  tokenActivities(tokenId: $tokenActivitiesTokenId2) {
+    
+  }
+  listings {
+    
+  }
+  offers {
+    
+  }
+  nameStatistics(tokenId: $nameStatisticsTokenId2) {
+    
+  }
+  chainStatistics {
+    
+  }
+  dailyActiveWallets(timeRange: $timeRange) {
+    
+  }
+  dailyTransactionCount(timeRange: $dailyTransactionCountTimeRange2) {
+    
+  }
+  dailyCumulativeTokenizationCount(timeRange: $dailyCumulativeTokenizationCountTimeRange2) {
+    
+  }
+  dailyTokenizationCount(timeRange: $dailyTokenizationCountTimeRange2) {
+    
+  }
+  dailyRevenue(timeRange: $dailyRevenueTimeRange2) {
+    
+  }
 }
